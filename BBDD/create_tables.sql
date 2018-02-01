2966bca95fe8cea0451c04f632e97e5a0f2eb4c9
DarFig <log.Dariel@gmail.com>
2018-02-01 21:38:54 +0100

    update tables

--- BBDD/create_tables.sql
+++ BBDD/create_tables.sql
@@ -4,7 +4,14 @@ use bdoMarket;
 CREATE TABLE IF NOT EXISTS objeto(
   id int unsigned AUTO_INCREMENT PRIMARY key,
   nombre varchar(40)
+  id_precio int unsigned,
+  index(id_precio),
+  foreign key(id_precio) reference precio (id) on delete cascade on update no action
+);
 
+CREATE TABLE IF NOT EXISTS proceso(
+  id int unsigned AUTO_INCREMENT PRIMARY key,
+  nombre varchar(40)
 );
 
 CREATE TABLE IF NOT EXISTS receta(
@@ -12,7 +19,10 @@ CREATE TABLE IF NOT EXISTS receta(
   nota varchar(300),
   resultado int unsigned,
   index(resultado),
-  foreign key(resultado) reference objeto (id) on delete cascade on update no action
+  foreign key(resultado) reference objeto (id) on delete cascade on update no action,
+  id_proceso int unsigned,
+  index(id_proceso),
+  foreign key(id_proceso) reference proceso (id) on delete cascade on update no action
 );
 
 CREATE TABLE IF NOT EXISTS ingrediente(
