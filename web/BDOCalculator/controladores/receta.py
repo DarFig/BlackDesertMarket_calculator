b26ece7b3df25548f9386abad287900ba278c149
DarFig <log.Dariel@gmail.com>
2018-03-15 18:43:59 +0100

    tratamiento form

--- web/BDOCalculator/controladores/receta.py
+++ web/BDOCalculator/controladores/receta.py
@@ -12,7 +12,7 @@ from wtforms import Form, BooleanField, validators
 from wtforms.validators import *
 
 from ..utils import *
-
+from ..formulas import is_profit
 
 #class LoCompramosForm(Form):
 #    comprar = BooleanField('comprar')
@@ -30,12 +30,32 @@ def recetadetails(pk):
 @app.route('/receta/<int:pk>/', methods=['POST'])
 def recetacompra(pk):
     objetos = get_objetos_por_receta(pk)
+    precios_producto = get_precio(get_object(get_receta(pk).resultado)
     formulario = request.form
-    print formulario
+    coste_max = 0
+    coste_min = 0
+    coste_1 = 0
+    coste_2 = 0
+    coste_3 = 0
+    coste_4 = 0
     for i in formulario:
-        print i
+        precio = get_precio(i) #precios del objetos
+        coste_max += precio.maximo
+        coste_min += precio.minimo
+        coste_1 += precio.precio1
+        coste_2 += precio.precio2
+        coste_3 += precio.precio3
+        coste_4 += precio.precio4
+    #toman el valor de si es profit
+    coste_max = is_profit(coste_max, precios_producto)
+    coste_min =  is_profit(coste_min, precios_producto)
+    coste_1 =  is_profit(coste_1, precios_producto)
+    coste_2 =  is_profit(coste_2, precios_producto)
+    coste_3 =  is_profit(coste_3, precios_producto)
+    coste_4 =  is_profit(coste_4, precios_producto)
 
-    #a partir de aqui no funcional, solo para la prueba||||
+    #a partir de aqui no funcional, solo para la prueba|||
+    #hay que devolver los que son profit
     receta = get_receta(pk)
     response = make_response(redirect(url_for('recetadetails', pk=pk)))
     return response
---
+++ web/BDOCalculator/formulas.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+def is_profit(coste_ingrediente, precios_producto):
+    #devuelve 0, o precio
+    #0 ->si no es profit
+    #precio -> a partir del cual es prifit
+    #es profit sin value, automaticamente con value tambien
+    #todo
+    return 1
+
+def is_profit_value(coste_ingrediente, precios_producto):
+    #devuelve 0, o precio
+    #0 ->si no es profit
+    #precio -> a partir del cual es prifit
+    #es profit con value
+    #todo
+    return 1
