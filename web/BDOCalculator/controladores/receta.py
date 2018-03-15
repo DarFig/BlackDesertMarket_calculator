456958720eab3b5d178772f6b32e0a1860d0bf88
DarFig <log.Dariel@gmail.com>
2018-03-15 17:28:40 +0100

    sol: array form

--- web/BDOCalculator/controladores/receta.py
+++ web/BDOCalculator/controladores/receta.py
@@ -3,6 +3,9 @@ from BDOCalculator import app
 
 from flask import render_template
 from flask import request
+from flask import make_response
+from flask import redirect
+from flask import url_for
 
 from flask_wtf import FlaskForm
 from wtforms import Form, BooleanField, validators
@@ -11,21 +14,28 @@ from wtforms.validators import *
 from ..utils import *
 
 
-class LoCompramosForm(Form):
-    comprar = BooleanField('comprar')
+#class LoCompramosForm(Form):
+#    comprar = BooleanField('comprar')
 
 @app.route('/receta/<int:pk>/', methods=['GET'])
 def recetadetails(pk):
-    formulario = LoCompramosForm()
     receta = get_receta(pk)
     objetos = get_objetos_por_receta(pk)
-    return render_template('_views/receta.html', receta=receta, objetos=objetos, loCompramosForm=formulario)
+    #formulario = {}
+    #for i in objetos:
+        #formulario[i.id] = LoCompramosForm()
+        #print formulario[i.id]
+    return render_template('_views/receta.html', receta=receta, objetos=objetos)
 
 @app.route('/receta/<int:pk>/', methods=['POST'])
 def recetacompra(pk):
-    formulario = LoCompramosForm(request.form)
+    objetos = get_objetos_por_receta(pk)
+    formulario = request.form
+    print formulario
+    for i in formulario:
+        print i
 
     #a partir de aqui no funcional, solo para la prueba||||
     receta = get_receta(pk)
-    objetos = get_objetos_por_receta(pk)
-    return render_template('_views/receta.html', receta=receta, objetos=objetos, loCompramosForm=formulario)
+    response = make_response(redirect(url_for('recetadetails', pk=pk)))
+    return response
