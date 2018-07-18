8ed0c2e54b886d5f365ea7a0cb6c14bc19b01c68
DarFig <log.Dariel@gmail.com>
2018-07-18 20:17:27 +0200

    read configFile

--- web/BDOCalculator/__init__.py
+++ web/BDOCalculator/__init__.py
@@ -6,8 +6,23 @@ import os
 
 app = Flask(__name__) #create the application instance
 
+import sys
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://bdocalculator:bdocalculator@localhost/bdocalculator"
+# revisión de la versión
+if sys.version[0] == '2' :
+    #2.7.x
+    import io
+    with io.open("configFile.txt", encoding="utf-8") as confFile:
+        data_base_uri = confFile.readline()
+        data_base_uri = data_base_uri.split()[1]
+else:
+    #3.x.x
+    with open("configFile.txt", encoding="utf-8") as confFile:
+        data_base_uri = confFile.readline()
+        data_base_uri = data_base_uri.split()[1]
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = data_base_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
