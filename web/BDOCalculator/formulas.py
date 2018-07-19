9fd57f6745ec2755f37585552a08b59204324464
DarFig <log.Dariel@gmail.com>
2018-07-20 00:40:59 +0200

    reduccion de la lista devuelta por las formulas

--- web/BDOCalculator/formulas.py
+++ web/BDOCalculator/formulas.py
@@ -10,43 +10,55 @@ def menosImpuesto_masValue(precio_venta):
 def precio_profit(coste_ingredientes, precios_producto):
     precioGanancia = []
     ganancia = menos_impuesto_market(precios_producto.maximo) - coste_ingredientes
-    precioGanancia.append({'precioMax' : precios_producto.maximo, 'profit' : ganancia})
+    if(ganancia > 0):
+        precioGanancia.append({'precioMax' : precios_producto.maximo, 'profit' : ganancia})
 
     ganancia = menos_impuesto_market(precios_producto.minimo) - coste_ingredientes
-    precioGanancia.append({'precioMin' : precios_producto.minimo, 'profit' : ganancia})
+    if(ganancia > 0):
+        precioGanancia.append({'precioMin' : precios_producto.minimo, 'profit' : ganancia})
 
     ganancia = menos_impuesto_market(precios_producto.precio1) - coste_ingredientes
-    precioGanancia.append({'precio1' : precios_producto.precio1, 'profit' : ganancia})
+    if(ganancia > 0):
+        precioGanancia.append({'precio1' : precios_producto.precio1, 'profit' : ganancia})
 
     ganancia = menos_impuesto_market(precios_producto.precio2) - coste_ingredientes
-    precioGanancia.append({'precio2' : precios_producto.precio2, 'profit' : ganancia})
+    if(ganancia > 0):
+        precioGanancia.append({'precio2' : precios_producto.precio2, 'profit' : ganancia})
 
     ganancia = menos_impuesto_market(precios_producto.precio3) - coste_ingredientes
-    precioGanancia.append({'precio3' : precios_producto.precio3, 'profit' : ganancia})
+    if(ganancia > 0):
+        precioGanancia.append({'precio3' : precios_producto.precio3, 'profit' : ganancia})
 
     ganancia = menos_impuesto_market(precios_producto.precio4) - coste_ingredientes
-    precioGanancia.append({'precio4' : precios_producto.precio4, 'profit' : ganancia})
+    if(ganancia > 0):
+        precioGanancia.append({'precio4' : precios_producto.precio4, 'profit' : ganancia})
 
     return precioGanancia
 
 def precio_profit_value(coste_ingredientes, precios_producto):
     precioGanancia = []
     ganancia = menosImpuesto_masValue(precios_producto.maximo) - coste_ingredientes
-    precioGanancia.append({'precioMax' : precios_producto.maximo, 'profit' : ganancia})
+    if(ganancia > 0):
+        precioGanancia.append({'precioMax' : precios_producto.maximo, 'profit' : ganancia})
 
     ganancia = menosImpuesto_masValue(precios_producto.minimo) - coste_ingredientes
-    precioGanancia.append({'precioMin' : precios_producto.minimo, 'profit' : ganancia})
+    if(ganancia > 0):
+        precioGanancia.append({'precioMin' : precios_producto.minimo, 'profit' : ganancia})
 
     ganancia = menosImpuesto_masValue(precios_producto.precio1) - coste_ingredientes
-    precioGanancia.append({'precio1' : precios_producto.precio1, 'profit' : ganancia})
+    if(ganancia > 0):
+        precioGanancia.append({'precio1' : precios_producto.precio1, 'profit' : ganancia})
 
     ganancia = menosImpuesto_masValue(precios_producto.precio2) - coste_ingredientes
-    precioGanancia.append({'precio2' : precios_producto.precio2, 'profit' : ganancia})
+    if(ganancia > 0):
+        precioGanancia.append({'precio2' : precios_producto.precio2, 'profit' : ganancia})
 
     ganancia = menosImpuesto_masValue(precios_producto.precio3) - coste_ingredientes
-    precioGanancia.append({'precio3' : precios_producto.precio3, 'profit' : ganancia})
+    if(ganancia > 0):
+        precioGanancia.append({'precio3' : precios_producto.precio3, 'profit' : ganancia})
 
     ganancia = menosImpuesto_masValue(precios_producto.precio4) - coste_ingredientes
-    precioGanancia.append({'precio4' : precios_producto.precio4, 'profit' : ganancia})
+    if(ganancia > 0):
+        precioGanancia.append({'precio4' : precios_producto.precio4, 'profit' : ganancia})
 
     return precioGanancia
