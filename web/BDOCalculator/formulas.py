5b87fe5f7b9c378526fac478e23b9a5c31f2c831
DarFig <log.Dariel@gmail.com>
2018-07-21 10:53:00 +0200

    formulas en modelo max-min

--- web/BDOCalculator/formulas.py
+++ web/BDOCalculator/formulas.py
@@ -7,7 +7,30 @@ def menosImpuesto_masValue(precio_venta):
     precio = menos_impuesto_market(precio_venta)
     return precio + precio * 0.3
 
+
 def precio_profit(coste_ingredientes, precios_producto):
+    precioGanancia = []
+
+    ganancia = menos_impuesto_market(precios_producto.maximo) - coste_ingredientes
+    precioGanancia.append({'precioMax' : precios_producto.maximo, 'profit' : ganancia})
+
+    ganancia = menos_impuesto_market(precios_producto.minimo) - coste_ingredientes
+    precioGanancia.append({'precioMin' : precios_producto.minimo, 'profit' : ganancia})
+
+    return precioGanancia
+
+def precio_profit_value(coste_ingredientes, precios_producto):
+    precioGanancia = []
+
+    ganancia = menosImpuesto_masValue(precios_producto.maximo) - coste_ingredientes
+    precioGanancia.append({'precioMax' : precios_producto.maximo, 'profit' : ganancia})
+
+    ganancia = menosImpuesto_masValue(precios_producto.minimo) - coste_ingredientes
+    precioGanancia.append({'precioMin' : precios_producto.minimo, 'profit' : ganancia})
+    
+    return precioGanancia
+
+def precio_profit_cpmpleto(coste_ingredientes, precios_producto):
     precioGanancia = []
     ganancia = menos_impuesto_market(precios_producto.maximo) - coste_ingredientes
     if(ganancia > 0):
@@ -35,7 +58,7 @@ def precio_profit(coste_ingredientes, precios_producto):
 
     return precioGanancia
 
-def precio_profit_value(coste_ingredientes, precios_producto):
+def precio_profit_value_completo(coste_ingredientes, precios_producto):
     precioGanancia = []
     ganancia = menosImpuesto_masValue(precios_producto.maximo) - coste_ingredientes
     if(ganancia > 0):
