25217c92b7d61c0d0eac972d075eaf3b41ad5e13
DarFig <log.Dariel@gmail.com>
2018-09-04 23:46:54 +0200

    upd requirements

--- inst.sh
+++ inst.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt-get install python-mysqldb
 sudo pip install virtualenv
 sudo apt-get install python-virtualenv
 virtualenv venv
+. venv/bin/activate
+pip install -r requirements.txt
